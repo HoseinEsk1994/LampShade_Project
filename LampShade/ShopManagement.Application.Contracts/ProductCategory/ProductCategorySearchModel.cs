a24d6b81b7fd527ace57fabeba0ee3440e73a959
Hosein <hosein.eskandariii1994@gmail.com>
2023-05-16 11:54:30 +0330

    Lampshde Project Initialized

---
+++ LampShade/ShopManagement.Application.Contracts/ProductCategory/ProductCategorySearchModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ShopManagement.Application.Contracts.ProductCategory
+{
+    public class ProductCategorySearchModel
+    {
+        public string Name { get; set; }
+    }
+}
